431dbeee4cca738532f0cf48df5b67c075eda7a0
Erik Rasmus Nilsson <info@rasmusnilsson.se>
2021-02-16 08:09:39 +0100

    cleaned up app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './app.scss';
 
-import CtaButton from './components/buttons/cta-button';
-
 const App = () => {
     return (
         <div className="app">
-            <CtaButton>Hello world</CtaButton>
         </div>
     );
 }
